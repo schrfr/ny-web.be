2f26aabde75ecba76fe5005e27623745859f8740
Alexandre Leray <alexandre@alexandreleray.com>
2013-05-17 01:26:37 +0200

    Mv media_root + static_root vers local_settings

--- ny/settings.py
+++ ny/settings.py
@@ -39,14 +39,7 @@ USE_I18N = False
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media/')
 MEDIA_URL = '/media/'
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticis')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
