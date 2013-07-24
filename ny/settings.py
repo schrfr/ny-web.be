0210ad6db2a29ff6e7d83262347ec3c53f246a40
Eric Schrijver <eric@ericschrijver.nl>
2013-07-24 21:26:49 +0200

    ( admin media prefix way of the dodo )

--- ny/settings.py
+++ ny/settings.py
@@ -61,11 +61,6 @@ STATICFILES_DIRS = (
 )
 
 
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
-
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
